936d7149a7a3564348a22d986435c3a9cb34e7c1
daca11 <daca11.n2o@gmail.com>
2017-03-18 13:09:23 +0100

    kill process + keypress

--- motion.py
+++ motion.py
@@ -1,31 +1,38 @@
 import glob
 import os
+import signal
+import subprocess
+import sys
+import time
 
 from watchdog.events import PatternMatchingEventHandler
 from watchdog.observers import Observer
 
-#TODO: read from config file
-MOTION_ROOT_DIR="/home/pi/motionfiles"
-CAMERAS=2
-PATTERN="*.avi"
+# TODO: read from config file
+MOTION_ROOT_DIR = "/home/pi/motionfiles"
+CAMERAS = 2
+PATTERN = "*.avi"
 
 """
     Moves all *.avi files of each camera into a directory
     named like the first video in the directory of that camera
 """
+
+
 def archiveFiles():
-    for num_cam in xrange(1,CAMERAS+1):
+    for num_cam in xrange(1, CAMERAS + 1):
         cam_dir = MOTION_ROOT_DIR + "/cam" + str(num_cam)
         new_dir = ""
         for file in sorted(glob.glob1(cam_dir, PATTERN)):
-            #file = only filename+ext
+            # file = only filename+ext
             if new_dir == "":
                 filename, extension = os.path.splitext(file)
                 new_dir = cam_dir + "/" + filename
-                os.mkdir(new_dir) #TODO: check if exists...
+                os.mkdir(new_dir)  # TODO: check if exists...
             print "rename " + cam_dir + "/" + file
             print "to " + new_dir + "/" + file
             os.rename(cam_dir + "/" + file, new_dir + "/" + file)
+
 
 class WatcherHandler(PatternMatchingEventHandler):
     patterns = [PATTERN]
@@ -47,32 +54,52 @@ class WatcherHandler(PatternMatchingEventHandler):
 
         fileList = sorted(glob.glob1(rootEventPath, PATTERN))
         if (len(fileList) > 2):
-            fileToRemove = rootEventPath + "/" + fileList.pop(0)  #TODO: join paths with os.join?
+            fileToRemove = rootEventPath + "/" + fileList.pop(0)  # TODO: join paths with os.join?
             print "removing " + fileToRemove
             os.remove(fileToRemove)
 
     def on_created(self, event):
         self.process(event)
 
-def observeFiles():
-    observer = Observer()
-    for num_cam in xrange(1, CAMERAS + 1):
-        # schedule a watcher for each camera
-        observer.schedule(WatcherHandler(), path=MOTION_ROOT_DIR + "/cam" + str(num_cam))
 
-    observer.start()
-
-    try:
-        while True:
-            pass
-            #time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-        observer.join()
+def killProcess(process):
+    os.killpg(os.getpgid(process.pid), signal.SIGTERM)
 
 
 # MAIN PROGRAM
 
 if __name__ == '__main__':
-    archiveFiles()
-    observeFiles()
+    p = None
+
+    observer = Observer()
+    # schedule a watcher for each camera
+    for num_cam in xrange(1, CAMERAS + 1):
+        observer.schedule(WatcherHandler(), path=MOTION_ROOT_DIR + "/cam" + str(num_cam))
+    observer.start()
+
+    try:
+        while True:
+            newEvent = False
+
+            archiveFiles()
+
+            print "Starting to record..."
+
+            p = subprocess.Popen("motion -m -c " + os.path.join(MOTION_ROOT_DIR, "configuration", "motion.conf"),
+                                 shell=True, preexec_fn=os.setsid)
+
+            while not newEvent:
+                key = sys.stdin.read()
+                if key == "q":
+                    newEvent = True
+                    print "New event! starting 1min countdown"
+                    time.sleep(60)
+                    print "Stopping motion..."
+                    killProcess(p)
+
+    finally:
+        print "HALTING DOWN!"
+        killProcess(p)
+        observer.stop()
+        observer.join()
+        print "HALTED!"
